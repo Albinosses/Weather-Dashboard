ddfad14fce2b3249f2e2407f9419cae05d6d68d8
Vasyl <vsyman9@gmail.com>
2024-08-29 02:03:02 +0300

    refactor: code cleanup

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -4,12 +4,10 @@ import styles from "./ErrorPage.module.css";
 const ErrorModal = () => {
   const error = useRouteError();
 
-  console.log(error.data);
-
   return (
     <div className={styles.container}>
       <h1>An error occured</h1>
-      <p>{error.data.message}</p>
+      {error.data && <p>{error.data.message}</p>}
       <Link to="/" className={styles.link}>
         Go back
       </Link>
